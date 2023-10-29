8882ab63fec3c24b0155bcc623fbf4575bb35b51
Adrian Ionut Patrascu <ap@greenbyte.dk>
2023-10-30 00:06:42 +0100

    refactoring/creating a resusable APi client

--- game-hub/src/services/api-client.ts
+++ game-hub/src/services/api-client.ts
@@ -1,14 +1,30 @@
-import axios from "axios";
-
+import axios, { AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
 
-export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
+const axiosInstance = axios.create({
+  baseURL: "https://api.rawg.io/api",
   params: {
     key: "9b9a86c4b2cf4f1997fc67976831e76e",
   },
 });
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+}
+
+
+export default APIClient
