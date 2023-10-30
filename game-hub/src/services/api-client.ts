f9ae2e3d99b23c98c57dbb74c131008576a50825
Adrian Ionut Patrascu <ap@greenbyte.dk>
2023-10-30 14:38:44 +0100

    Fetch game details

---
+++ game-hub/src/services/api-client.ts
@@ -0,0 +1,36 @@
+import axios, { AxiosRequestConfig } from "axios";
+
+export interface FetchResponse<T> {
+  count: number;
+  next: string | null;
+  results: T[];
+}
+
+const axiosInstance = axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "bd7905414f1c48619d2c6216327fb075",
+  },
+});
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+
+  get = (id: number | string) => {
+    return axiosInstance
+      .get<T>(this.endpoint + "/" + id)
+      .then((res) => res.data);
+  };
+}
+
+export default APIClient;
