00cfbcd6e8ff2b1442152c5cab28821718a4db22
Adrian Ionut Patrascu <ap@greenbyte.dk>
2023-10-30 14:11:37 +0100

    Handle errors

--- game-hub/src/routes.tsx
+++ game-hub/src/routes.tsx
@@ -1,22 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
+import GameDetailPage from "./pages/GameDetailPage";
 import HomePage from "./pages/HomePage";
 import Layout from "./pages/Layout";
-import GameDetailPage from "./pages/GameDetailPage";
-
-
-
-
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Layout />,
-        children: [
-            { index: true, element: <HomePage /> },
-            {path: 'games/:id', element: <GameDetailPage />}
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "games/:slug", element: <GameDetailPage /> },
+    ],
+  },
+]);
 
-
-export default router
+export default router;
